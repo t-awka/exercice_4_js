08272b62746c851db099f6b158be61bed37c33cc
t-awka <80962656+t-awka@users.noreply.github.com>
2021-04-29 13:32:41 +0200

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+let prenom = prompt("Quel est ton prenom?");
+alert("Bonjour " + prenom);
+
+let age = prompt("Quel est votre age?");
+alert("Tu as " + age + " ans");
+console.log(age);
+
+let prenom = prompt("Donne moi ton prenom");
+let nom = prompt("Donne moi ton nom");
+let completeNom = prenom + " " + nom;
+alert("Salut " + completeNom);
+
+let x = prompt("Ton prenom?").toUpperCase() + " " + prompt("Ton nom?");
+alert("Bonjour " + x);
